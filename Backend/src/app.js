59daf32bc774049fead7b05d55a15ce5653227a7
Kushagra Bharti <kushagrabharti04@gmail.com>
2026-02-09 23:07:06 +0530

    Prisma migration done

---
+++ Backend/src/app.js
@@ -0,0 +1,22 @@
+import express from "express"
+import cors from "cors"
+
+import { config } from "./config/config.js"
+
+const app = express()
+
+const PORT = config.PORT
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Server up!"
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
